cc72a324eb8d4ecafb789ee87145f46aaca97ce5
kanogame <wasdqert1222@gmail.com>
2023-12-13 21:33:57 +0700

    day 1 completed (refactoring needed)

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,49 @@ use std::fs::read_to_string;
 fn main() {
     let input = read_lines("data.txt");
     
+    part_two(input);
+}
+
+fn part_two(input: Vec<String>) {
+    let nums = [
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven", 
+        "eight",
+        "nine"
+    ];
+
+    let mut res = 0;
+    for line in input {
+        let mut first = 10;
+        let mut last = 0;
+        let mut offset: usize = 0;
+        for (i, data) in line.chars().enumerate() {
+            for (j, num) in nums.iter().enumerate() {
+                if line[offset..i + 1].contains(num) {
+                    if first == 10 {
+                        first = j + 1;
+                    }
+                    last = j + 1;
+                    offset = i;
+                } else if data.is_digit(10) == true {
+                    if first == 10 {
+                        first = data.to_digit(10).unwrap() as usize;
+                    }
+                    last = data.to_digit(10).unwrap() as usize;
+                }
+            }
+        }
+        res += first * 10 + last
+    }
+    println!("{}", res);
+}
+
+fn part_one(input: Vec<String>) {
     let mut res = 0;
     let mut first = 10;
     let mut last = 0;
