ae35fe742bff63812819078feddab7ef320479ee
kanogame <wasdqert1222@gmail.com>
2023-12-14 16:29:48 +0700

    day 2 - part 2

--- p2/src/main.rs
+++ p2/src/main.rs
@@ -4,6 +4,29 @@ use regex::Regex;
 
 fn main() {
     let input = read_lines("data.txt");
+    part_two(input);
+}
+
+fn part_two(input: Vec<String>) {
+    let line = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
+    let re = Regex::new("([0-9]*).(blue|red|green)").unwrap();
+    let mut res = 0;
+    for (i, line) in input.into_iter().enumerate() {
+        let mut minBlue = 0;
+        let mut minGreen = 0;
+        let mut minRed = 0;
+        for (_, [num, color]) in re.captures_iter(&line).map(|c| c.extract()) {
+            let num = num.parse::<i32>().unwrap();
+            match color {
+                "blue" => if num > minBlue {minBlue = num}
+                "green" => if num > minGreen {minGreen = num}
+                "red" => if num > minRed {minRed = num}
+                other => ()
+            }
+        }
+        res += minBlue * minGreen * minRed;
+    }
+    println!("{}", res)
 }
 
 fn part_one(input: Vec<String>) {
