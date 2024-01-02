52ed7cd1c0ab779823f065655ab0538bb0161596
kanogame <wasdqert1222@gmail.com>
2024-01-02 15:43:23 +0700

    day 9 pt2 aka worst solution ever

--- p9/src/main.rs
+++ p9/src/main.rs
@@ -7,11 +7,36 @@ fn main() {
 }
 
 fn solution_pt1(lines: Vec<Vec<i32>>) {
-    let res = lines.iter().map(|i| solve_line(i) as i64).collect::<Vec<i64>>().iter().sum::<i64>();
+    let res = lines.iter().map(|i| solve_line_pt2(i) as i64).collect::<Vec<i64>>().iter().sum::<i64>();
     print!("{:?}", res);
 }
 
-fn solve_line(dataset: &Vec<i32>) -> i32 {
+fn solve_line_pt2(dataset: &Vec<i32>) -> i32 {
+    let mut data: Vec<Vec<i32>> = Vec::new();
+    data.push(dataset.clone());
+
+    let mut newarr: Vec<i32> = Vec::new();
+    let mut prewarr = dataset.clone();
+    while !check_vec(&prewarr) {
+        for i in 0..prewarr.len() - 1 {
+            newarr.push(prewarr[i + 1] - prewarr[i])
+        }
+        data.push(newarr.clone());
+        prewarr = newarr;
+        newarr = Vec::new();
+    }
+
+    let temp = data.last().unwrap()[0].clone();
+    data.iter_mut().last().unwrap().push(temp);
+    for i in 2..data.len() + 1 {
+        let j = data.len() - i;
+        let temp = data[j][0] - data[j + 1].last().unwrap();
+        data[j].push(temp);
+    }
+    *data[0].last().unwrap()
+}
+
+fn solve_line_pt1(dataset: &Vec<i32>) -> i32 {
     let mut res = 0;
 
     let mut newarr: Vec<i32> = Vec::new();
